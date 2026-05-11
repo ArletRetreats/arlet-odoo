2f2fcf34c0f5de7751180a4113ee4fbb16000780
jason13657 <jong13657@gmail.com>
2026-05-11 22:46:48 +1000

    chore: upgrade to Odoo 19.0

--- addons/arlet_cms/__manifest__.py
+++ addons/arlet_cms/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Arlet CMS',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Website/CMS',
     'summary': 'CMS data models and REST API for Arlet Next.js frontend',
     'description': (
